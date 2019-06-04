e4a8e26c501784077d8057b123800ea19839061f
Fred Roth <f-roth@megaera.de>
2019-06-04 14:15:48 +0200

    Adds syntactic sugar for immutable.Document

--- src/main/scala/medeia/syntax/BsonEncoderSyntax.scala
+++ src/main/scala/medeia/syntax/BsonEncoderSyntax.scala
@@ -4,12 +4,18 @@ import cats.data.EitherNec
 import medeia.decoder.{BsonDecoder, BsonDecoderError}
 import medeia.encoder.BsonEncoder
 import org.mongodb.scala.bson.BsonValue
+import org.mongodb.scala.bson.collection.immutable.Document
 
 trait BsonEncoderSyntax {
   implicit class BsonEncoderOps[A: BsonEncoder](a: A) {
     def toBson: BsonValue = BsonEncoder[A].encode(a)
   }
+
   implicit class BsonDecoderOps(bsonValue: BsonValue) {
     def fromBson[A: BsonDecoder]: EitherNec[BsonDecoderError, A] = BsonDecoder.decode(bsonValue)
+  }
+
+  implicit class BsonDecoderOpsForDocument(document: Document) {
+    def fromBson[A: BsonDecoder]: EitherNec[BsonDecoderError, A] = BsonDecoder.decode(document.toBsonDocument)
   }
 }
