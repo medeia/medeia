ee92d5ebd222a5973b6ab981eb4081de233d2774
Fred Roth <f-roth@megaera.de>
2019-06-09 11:45:04 +0200

    Updated Scala Version to 2.12.8

--- project/MiscSettingsPlugin.scala
+++ project/MiscSettingsPlugin.scala
@@ -10,7 +10,7 @@ object MiscSettingsPlugin extends AutoPlugin {
 
   lazy val commonSettings = Seq(
     organization := "",
-    scalaVersion := "2.12.7",
+    scalaVersion := "2.12.8",
     scalacOptions ++= Seq(
       "-unchecked",
       "-deprecation",
