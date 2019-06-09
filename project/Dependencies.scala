a68d5a01a67ca67a46b0346a3f625ebfda86a063
Fred Roth <f-roth@megaera.de>
2019-06-09 13:40:46 +0200

    Updated cats version to 1.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val scalaTest = "3.0.5"
     val mongoScalaBson = "2.6.0"
-    val cats = "1.6.0"
+    val cats = "1.6.1"
     val scalaCheck = "1.14.0"
     val shapeless = "2.3.3"
   }
