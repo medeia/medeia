f3286bc9481bfaeb92d16ae67e3f9dfdae7c6176
Fred Roth <f-roth@megaera.de>
2019-11-28 20:27:15 +0100

    Updates scalacheck version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scalaTest = "3.0.8"
     val mongoScalaBson = "2.7.0"
     val cats = "2.0.0"
-    val scalaCheck = "1.14.0"
+    val scalaCheck = "1.14.2"
     val shapeless = "2.3.3"
     val scalaCollectionCompatVersion = "2.1.2"
   }
