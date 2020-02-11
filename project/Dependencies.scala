a08755299940824abf801b3bcc61fbc08e147e1b
Scala Steward <me@scala-steward.org>
2020-02-11 10:48:14 +0100

    Update scala-collection-compat to 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val cats = "2.1.0"
     val scalaCheck = "1.14.3"
     val shapeless = "2.3.3"
-    val scalaCollectionCompatVersion = "2.1.3"
+    val scalaCollectionCompatVersion = "2.1.4"
   }
 
   lazy val Libraries = libraryDependencies ++= Vector(
