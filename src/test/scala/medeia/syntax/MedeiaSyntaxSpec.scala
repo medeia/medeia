2bb1edef6eb3f2ce40c30958a2929fb7bfc039a8
Fred Roth <f-roth@megaera.de>
2020-03-05 11:43:13 +0100

    Makes coproduct derivation more uniform

--- src/test/scala/medeia/syntax/MedeiaSyntaxSpec.scala
+++ src/test/scala/medeia/syntax/MedeiaSyntaxSpec.scala
@@ -4,10 +4,9 @@ import cats.data.NonEmptyChain
 import medeia.MedeiaSpec
 import medeia.decoder.BsonDecoder
 import medeia.decoder.BsonDecoderError.KeyNotFound
-import medeia.encoder.{BsonDocumentEncoder, BsonEncoder}
-import medeia.generic.GenericEncoder
-import org.mongodb.scala.bson.{BsonDocument, BsonString}
+import medeia.encoder.BsonDocumentEncoder
 import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.bson.{BsonDocument, BsonString}
 
 class MedeiaSyntaxSpec extends MedeiaSpec {
   behavior of classOf[MedeiaSyntax].getSimpleName
@@ -46,7 +45,12 @@ sealed trait Trait
 case class Foo(answer: Int) extends Trait
 case class Bar(bar: String) extends Trait
 object Trait {
-  implicit val traitEncoder: GenericEncoder[Trait] = implicitly //Does NPE
-  // implicit val traitEncoder: BsonDocumentEncoder[Trait] = medeia.generic.semiauto.deriveBsonEncoder[Trait] //DOES NOT COMPILE
+  implicit val fooEncoder: BsonDocumentEncoder[Foo] = medeia.generic.semiauto.deriveBsonEncoder
+  implicit val fooDecoder: BsonDecoder[Foo] = medeia.generic.semiauto.deriveBsonDecoder
+
+  implicit val barEncoder: BsonDocumentEncoder[Bar] = medeia.generic.semiauto.deriveBsonEncoder
+  implicit val barDecoder: BsonDecoder[Bar] = medeia.generic.semiauto.deriveBsonDecoder
+
+  implicit val traitEncoder: BsonDocumentEncoder[Trait] = medeia.generic.semiauto.deriveBsonEncoder[Trait]
   implicit val traitDecoder: BsonDecoder[Trait] = medeia.generic.semiauto.deriveBsonDecoder[Trait]
 }
