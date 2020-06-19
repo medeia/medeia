b46764a0abeb1334c45cebb5442c6cd31e5fd4ce
Scala Steward <me@scala-steward.org>
2020-06-19 05:37:14 +0200

    Update scalatest to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val scalaTest = "3.1.2"
+    val scalaTest = "3.2.0"
     val mongoScalaBson = "4.0.4"
     val cats = "2.1.1"
     val scalaCheck = "1.14.3"
