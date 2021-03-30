fcdcb79704f39ba33b7fa9983c351f5585d444e6
Scala Steward <me@scala-steward.org>
2021-03-30 04:31:56 +0200

    Update refined, refined-scalacheck to 0.9.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val shapeless = "2.3.3"
     val scalaCollectionCompatVersion = "2.4.2"
     val enumeratumVersion = "1.6.1"
-    val refinedVersion = "0.9.21"
+    val refinedVersion = "0.9.22"
   }
 
   lazy val Libraries: Vector[ModuleID] =  Vector(
