7d04690523f46555ef29b41d796a0afc8335452f
Markus Hauck <markus1189@gmail.com>
2021-05-12 19:55:17 +0200

    Add ErrorStack to BsonDecoderError
    This allows us to provide better help on errors, by providing a
    "Stack" that directly points to the error. For example:
    
    ```
    ErrorStack(List(Attr("bar"), Attr("baz"), Index(2), Case("Qux"), Attr("answer")))
    ```
    
    says that we looked into the case class attribute `bar.baz`, at index
    `2`, the sealed trait case `Qux`, the case class attribute `answer`
    and that's where the error occurred.

--- core/src/main/scala/medeia/generic/HlistDecoderInstances.scala
+++ core/src/main/scala/medeia/generic/HlistDecoderInstances.scala
@@ -1,11 +1,11 @@
 package medeia.generic
 
-import cats.data.{EitherNec, NonEmptyChain}
+import cats.data.NonEmptyChain
+import cats.syntax.either._
 import cats.syntax.parallel._
+import medeia.decoder.BsonDecoder
 import medeia.decoder.BsonDecoderError.KeyNotFound
-import medeia.decoder.{BsonDecoder, BsonDecoderError}
-import medeia.generic.util.VersionSpecific.Lazy
-import org.mongodb.scala.bson.BsonDocument
+import medeia.decoder.StackFrame.Attr
 import shapeless.labelled.{FieldType, field}
 import shapeless.{::, HList, HNil, Witness}
 
@@ -14,18 +14,19 @@ trait HlistDecoderInstances {
 
   implicit def hlistObjectDecoder[Base, K <: Symbol, H, T <: HList](implicit
       witness: Witness.Aux[K],
-      hDecoder: Lazy[BsonDecoder[H]],
+      hDecoder: BsonDecoder[H],
       tDecoder: ShapelessDecoder[Base, T],
       options: GenericDerivationOptions[Base] = GenericDerivationOptions[Base]()
-  ): ShapelessDecoder[Base, FieldType[K, H] :: T] = {
-    val fieldName: String = options.transformKeys(witness.value.name)
-    bsonDocument: BsonDocument => {
-      val head: EitherNec[BsonDecoderError, FieldType[K, H]] = Option(bsonDocument.get(fieldName)) match {
-        case Some(headField) => hDecoder.value.decode(headField).map(field[K](_))
-        case None            => hDecoder.value.defaultValue.map(field[K](_)).toRight(NonEmptyChain(KeyNotFound(fieldName)))
+  ): ShapelessDecoder[Base, FieldType[K, H] :: T] =
+    bsonDocument => {
+      val fieldName: String = options.transformKeys(witness.value.name)
+
+      val head = Option(bsonDocument.get(fieldName)) match {
+        case Some(headField) => hDecoder.decode(headField).map(field[K](_)).leftMap(_.map(_.push(Attr(fieldName))))
+        case None            => hDecoder.defaultValue.map(field[K](_)).toRight(NonEmptyChain(KeyNotFound(fieldName)))
       }
       val tail = tDecoder.decode(bsonDocument)
+
       (head, tail).parMapN(_ :: _)
     }
-  }
 }
