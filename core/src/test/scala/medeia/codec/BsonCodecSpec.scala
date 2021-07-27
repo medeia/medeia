1b2f12d9f74d22e81deb13ec988030fe08d3946d
Leonhard Riedisser <leo008180@googlemail.com>
2021-07-27 14:12:40 +0200

    Add tests for iemap implementations

--- core/src/test/scala/medeia/codec/BsonCodecSpec.scala
+++ core/src/test/scala/medeia/codec/BsonCodecSpec.scala
@@ -1,6 +1,8 @@
 package medeia.codec
 
+import cats.syntax.either._
 import medeia.MedeiaSpec
+import medeia.decoder.BsonDecoderError.GenericDecoderError
 
 class BsonCodecSpec extends MedeiaSpec {
   behavior of "BsonCodec"
@@ -14,5 +16,28 @@ class BsonCodecSpec extends MedeiaSpec {
     val result = imappedCodec.decode(imappedCodec.encode(input))
 
     result should ===(Right(input))
+  }
+
+  it should "support iemap" in {
+    val codec = BsonCodec[Int]
+    val iemappedCodec: BsonCodec[String] = codec.iemap(i => Right(i.toString))(_.toInt)
+
+    val input = "42"
+
+    val result = iemappedCodec.decode(iemappedCodec.encode(input))
+
+    result should ===(Right(input))
+  }
+
+  it should "handle failures in iemap" in {
+    val codec = BsonCodec[Int]
+
+    val error = "oops"
+
+    val iemappedCodec: BsonCodec[String] = codec.iemap(_ => Left(error))(_.toInt)
+
+    val result = iemappedCodec.decode(iemappedCodec.encode("42"))
+
+    result should ===(Left(GenericDecoderError(error)).toEitherNec)
   }
 }
--- core/src/test/scala/medeia/codec/BsonKeyCodecSpec.scala
+++ core/src/test/scala/medeia/codec/BsonKeyCodecSpec.scala
@@ -1,6 +1,8 @@
 package medeia.codec
 
 import medeia.MedeiaSpec
+import medeia.decoder.BsonDecoderError.GenericDecoderError
+import cats.syntax.either._
 
 class BsonKeyCodecSpec extends MedeiaSpec {
   behavior of "BsonKeyCodec"
@@ -14,5 +16,28 @@ class BsonKeyCodecSpec extends MedeiaSpec {
     val result = imappedCodec.decode(imappedCodec.encode(input))
 
     result should ===(Right(input))
+  }
+
+  it should "support iemap" in {
+    val codec = BsonKeyCodec[Int]
+    val iemappedCodec: BsonKeyCodec[String] = codec.iemap(i => Right(i.toString))(_.toInt)
+
+    val input = "42"
+
+    val result = iemappedCodec.decode(iemappedCodec.encode(input))
+
+    result should ===(Right(input))
+  }
+
+  it should "handle failures in iemap" in {
+    val codec = BsonKeyCodec[Int]
+
+    val error = "oops"
+
+    val iemappedCodec: BsonKeyCodec[String] = codec.iemap(_ => Left(error))(_.toInt)
+
+    val result = iemappedCodec.decode(iemappedCodec.encode("42"))
+
+    result should ===(Left(GenericDecoderError(error)).toEitherNec)
   }
 }
