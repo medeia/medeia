c5e7cc69ed62a8f9e2e8f9724d7b3b1d07a616b1
Fred Roth <f-roth@megaera.de>
2021-09-07 16:16:35 +0200

    Support for ValueEnumEntry and keyCodec for EnumEntry

--- modules/enumeratum/src/test/scala/medeia/enumeratum/EnumeratumDecoderSpec.scala
+++ modules/enumeratum/src/test/scala/medeia/enumeratum/EnumeratumDecoderSpec.scala
@@ -1,7 +1,7 @@
 package medeia.enumeratum
 
 import medeia.decoder.BsonDecoderError.FieldParseError
-import org.mongodb.scala.bson.BsonString
+import org.mongodb.scala.bson.{BsonInt32, BsonString}
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpecLike
@@ -10,9 +10,15 @@ import org.scalatest.matchers.should.Matchers
 class EnumeratumDecoderSpec extends AnyFlatSpecLike with TypeCheckedTripleEquals with Matchers with EitherValues {
   behavior of "EnumeratumBsonDecoder"
 
-  it should "fail gracefully on unknown entry" in {
+  it should "Enum: fail gracefully on unknown entry" in {
     val unknown = "unknown"
 
     Enumeratum.decoder(TestEnum).decode(BsonString(unknown)).left.value.head shouldBe a[FieldParseError]
+  }
+
+  it should "IntEnum: fail gracefully on unknown entry" in {
+    val unknown = 42
+
+    Enumeratum.valueEnumDecoder(TestIntEnum).decode(BsonInt32(unknown)).left.value.head shouldBe a[FieldParseError]
   }
 }
