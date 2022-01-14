8350a513f4454645d0e440df56a972ca9957198c
Scala Steward <me@scala-steward.org>
2022-01-14 07:15:32 +0100

    Update scala-library to 2.13.8

--- project/MiscSettingsPlugin.scala
+++ project/MiscSettingsPlugin.scala
@@ -31,7 +31,7 @@ object MiscSettingsPlugin extends AutoPlugin {
 
   lazy val commonSettings = Seq(
     organization := "",
-    scalaVersion := "2.13.7",
+    scalaVersion := "2.13.8",
     scalacOptions ++= {
       CrossVersion.partialVersion(scalaVersion.value) match {
         case Some((2, 12)) => extraScalacOptions ++ extraScalacOptions_2_12
