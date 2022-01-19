e75586e6acbb7debc8b1dae8d7e0a82c16c9948e
Fred Roth <f-roth@megaera.de>
2022-01-19 17:26:43 +0100

    Remove unused OptionValues as scala-3 compiler does not like them

--- core/src/test/scala/medeia/MedeiaSpec.scala
+++ core/src/test/scala/medeia/MedeiaSpec.scala
@@ -3,6 +3,6 @@ package medeia
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.{EitherValues, OptionValues}
+import org.scalatest.EitherValues
 
-trait MedeiaSpec extends AnyFlatSpecLike with TypeCheckedTripleEquals with Matchers with EitherValues with OptionValues
+trait MedeiaSpec extends AnyFlatSpecLike with TypeCheckedTripleEquals with Matchers with EitherValues
