ca39397c51d61844ac145048e02b5a30d7208a2e
Fred Roth <f-roth@megaera.de>
2022-01-19 17:23:14 +0100

    Moved scala-2 specific code to scala-2 source folder

--- core/src/main/scala/medeia/generic/GenericEncoder.scala
+++ core/src/main/scala/medeia/generic/GenericEncoder.scala
@@ -1,18 +1,7 @@
 package medeia.generic
 
 import medeia.encoder.BsonDocumentEncoder
-import medeia.generic.util.VersionSpecific.Lazy
-import org.mongodb.scala.bson.BsonDocument
-import shapeless.LabelledGeneric
 
 trait GenericEncoder[A] extends BsonDocumentEncoder[A]
 
 object GenericEncoder extends GenericEncoderInstances
-
-trait GenericEncoderInstances {
-  implicit def genericEncoder[Base, H](implicit
-      generic: LabelledGeneric.Aux[Base, H],
-      hEncoder: Lazy[ShapelessEncoder[Base, H]]
-  ): GenericEncoder[Base] =
-    value => hEncoder.value.encode(generic.to(value), BsonDocument())
-}
