62b76b642bd7a71ccf10ec0b7ce638f91326da3a
Scala Steward <me@scala-steward.org>
2022-03-24 01:12:15 +0100

    Update mongo-scala-bson to 4.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val scalaTest = "3.2.11"
-    val mongoScalaBson = "4.5.0"
+    val mongoScalaBson = "4.5.1"
     val cats = "2.7.0"
     val scalaCheck = "1.15.4"
     val shapeless2 = "2.3.8"
