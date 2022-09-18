13612f802a15bb91e3c2c0ca85d19b864f7ff033
scala-steward <scala-steward@megaera.de>
2022-09-18 02:35:05 +0000

    Update shapeless3-deriving to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val cats = "2.8.0"
     val scalaCheck = "1.16.0"
     val shapeless2 = "2.3.10"
-    val shapeless3 = "3.1.0"
+    val shapeless3 = "3.2.0"
     val scalaCollectionCompatVersion = "2.8.1"
     val enumeratumVersion = "1.7.0"
     val refinedVersion = "0.10.1"
