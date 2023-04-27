216453500b0c452f75d04b9b51e73e2c3f5deff5
Scala Steward <scala_steward@virtuslab.com>
2023-04-27 16:31:28 +0000

    Update scala-collection-compat to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val scalaCheck = "1.17.0"
     val shapeless2 = "2.3.10"
     val shapeless3 = "3.3.0"
-    val scalaCollectionCompatVersion = "2.9.0"
+    val scalaCollectionCompatVersion = "2.10.0"
     val enumeratumVersion = "1.7.2"
     val refinedVersion = "0.10.3"
   }
