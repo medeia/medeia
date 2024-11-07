c089af4e2af05c48ce82575b6a4ce5937e5fb2e9
Scala Steward <scala_steward@virtuslab.com>
2024-11-07 17:09:13 +0000

    Update mongo-scala-bson to 5.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val scalaTest = "3.2.19"
-    val mongoScalaBson = "5.2.0"
+    val mongoScalaBson = "5.2.1"
     val cats = "2.12.0"
     val scalaCheck = "1.18.1"
     val shapeless2 = "2.3.12"
