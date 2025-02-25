5da1e07cd3bd6e604779be5140d73f22ad3c226b
Frederick Roth <f-roth@megaera.de>
2025-02-25 17:40:26 +0100

    scalafmt

--- core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
+++ core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
@@ -7,9 +7,9 @@ import org.bson.{BsonDocument, BsonValue}
 
 trait BsonDocumentCodecVersionSpecific {
   def derived[A](implicit genericEncoder: GenericEncoder[A], genericDecoder: GenericDecoder[A]): BsonDocumentCodec[A] = new BsonDocumentCodec[A] {
-      override def encode(a: A): BsonDocument = genericEncoder.encode(a)
+    override def encode(a: A): BsonDocument = genericEncoder.encode(a)
 
-      override def decode(bson: BsonValue): EitherNec[BsonDecoderError, A] =
-        genericDecoder.decode(bson)
+    override def decode(bson: BsonValue): EitherNec[BsonDecoderError, A] =
+      genericDecoder.decode(bson)
   }
 }
