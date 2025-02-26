e1d06e26d2bb77e947ca5ccee0b18361ef94f518
Frederick Roth <f-roth@megaera.de>
2025-02-26 08:46:16 +0100

    Cleanup

--- core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
+++ core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
@@ -1,9 +1,6 @@
 package medeia.codec
 
-import medeia.decoder.BsonDecoderError
 import medeia.generic.{GenericEncoder, GenericDecoder}
-import cats.data.EitherNec
-import org.bson.{BsonDocument, BsonValue}
 
 trait BsonDocumentCodecVersionSpecific {
   def derived[A](implicit genericEncoder: GenericEncoder[A], genericDecoder: GenericDecoder[A]): BsonDocumentCodec[A] =
