2fbb0c5b6ba6257d1d2a552dcf84d99c32426083
Frederick Roth <f-roth@megaera.de>
2025-02-26 08:30:44 +0100

    Add single recursion derive for scala 2.13.
    Plus: cleanup and privatizing of methods

--- core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
+++ core/src/main/scala-2/medeia/codec/BsonDocumentCodecVersionSpecific.scala
@@ -6,10 +6,6 @@ import cats.data.EitherNec
 import org.bson.{BsonDocument, BsonValue}
 
 trait BsonDocumentCodecVersionSpecific {
-  def derived[A](implicit genericEncoder: GenericEncoder[A], genericDecoder: GenericDecoder[A]): BsonDocumentCodec[A] = new BsonDocumentCodec[A] {
-    override def encode(a: A): BsonDocument = genericEncoder.encode(a)
-
-    override def decode(bson: BsonValue): EitherNec[BsonDecoderError, A] =
-      genericDecoder.decode(bson)
-  }
+  def derived[A](implicit genericEncoder: GenericEncoder[A], genericDecoder: GenericDecoder[A]): BsonDocumentCodec[A] =
+    BsonDocumentCodec.fromEncoderAndDecoder(genericEncoder, genericDecoder)
 }
